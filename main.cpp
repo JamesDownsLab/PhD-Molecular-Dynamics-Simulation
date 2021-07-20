f5d79da650bf5f9e6fac82e5ace9b37a9548a213
James <jamesdownslab@gmail.com>
2021-07-20 14:08:44 +0100

    Added command line argument parsing

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include "Engine.h"
-//#include "setup.h"
+#include <sstream>
+#include <string>
 
-int main() {
+int main(int argc, char** argv){
+    int steps{1000};
+    int save_interval{100};
+    for (int i = 0; i<argc; i++){
+        std::cout << argv[i] << "\n";
+        std::string command = argv[i];
+        if (command == "--steps"){
+            steps = atoi(argv[i+1]);
+            i++;
+        }
+        if (command == "--save_interval"){
+            save_interval = atoi(argv[i+1]);
+            i++;
+        }
+    }
+
     ProgramOptions options{
             "data.dump",
-            2000
+            save_interval
     };
 //    setup_experiment(0.8);
     Engine engine("initial.data", options);
     engine.set_baseplate(2e-4, 0.02);
-    for (int s{0}; s < 60000; s++) {
+    for (int s{0}; s <= steps; s++) {
         engine.step();
     }
 }
